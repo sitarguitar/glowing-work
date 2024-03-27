8de24bc9c8d70f0fb9cbd87a1a2f17718235cada
Wally Brennan <165150219+sitarguitar@users.noreply.github.com>
2024-03-27 11:55:37 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+ function runLength(vector) {
+      var result = [];
+      var zeros = 0;
+      var zerosTemp = '';
+      var i = 0;
+      for (; i < vector.length; i += 1) {
+        if (vector[i] === '0') {
+          zeros += 1;
+        } else {
+          zerosTemp = zeros.toString(2);
+          result.push(new Array(zerosTemp.length).join('1'));
+          result.push('0' + zerosTemp);
+          zeros = 0;
+        }
+      }
+      return result.join('');
+    }
+
+ return function (str) {
+      var asciiString = convertToAscii(str);
+      return runLength(asciiString);
+    };
+
+  }());
+
+  exports.runLength = runLengthEncoding;
+
+}(typeof exports === 'undefined' ? window : exports));
